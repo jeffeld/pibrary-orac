481ab655a75c52cc18e401d9d531f975169e0fab
Jeff Eldridge <eldridge.jeff@gmail.com>
2014-11-10 13:42:11 +0000

    BUGFIX: Changed default mode to production to ease deployment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var express = require('express');
  */
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 // Application Config
 var config = require('./lib/config/config');
